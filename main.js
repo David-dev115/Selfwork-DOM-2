40ff9844fa90e73c4bf250a675394899081aa1b8
David Paparo <d.paparo@tiscali.it>
2025-02-13 14:33:19 +0100

    carico esercizio completo

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+
+// Selfwork DOM 2
+
+// Crea un file html con le seguenti caratteristiche:
+// un input per il titolo
+// una textarea per inserire un paragrafo
+// un bottone per creare l’articolo
+
+// Al click sul bottone, crea un articolo popolato dai valori prelevati dai due input
+// inserire nell’articolo anche la data di pubblicazione tramite questa istruzione → Date - JavaScript | MDN
+
+// let date = new Date();
+// let formatDate = date.toLocaleDateString()
+
+// EXTRA:
+// fai in modo che, cliccando sul bottone crea articolo, se titolo o paragrafo sono vuoti, esca un alert che informi l’utente del problema
+// fai in modo che, una volta creato l’articolo, gli input vengano puliti
+
+
+let titolo = document.querySelector(`#titolo`);
+
+let pubblica_tutto = document.querySelector(`#pubblica_tutto`);
+
+let articolo = document.querySelector(`#articolo`);
+
+let wrapper = document.querySelector(`#wrapper`);
+
+
+wrapper.innerHTML = `<h1>hello world</h1>`;
+
+
+let date = new Date();
+// let formatDate = date.toLocaleDateString();
+
+
+
+
+pubblica_tutto.addEventListener( `click`, ()=>{
+    
+    if (titolo.value === `` || articolo.value === ``) {
+
+        alert(`Titolo o Articolo vuoti`);
+
+    } else {
+        
+        wrapper.innerHTML = `<h1>${titolo.value}</h1>`;
+        
+        wrapper.innerHTML += `<p>${articolo.value}</p>`;
+        
+        wrapper.innerHTML +=  `<p>${date}</p>`;
+
+        titolo.value = ``;
+
+        articolo.value = ``;
+        
+    }
+    
+    
+} );
+
+
+
+
+// min 53:00
+// min 1:21:00
+
+
+console.dir(titolo);
